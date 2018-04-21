ca96cd00f8a16d003c92f6a5fc0eb949c5fef40c
Martin Morales <themartin93@gmail.com>
2018-04-21 12:29:55 -0500

    Primera carga de archivos -Agregado iconos para el menu (configuraciones y acerca de) -Vinculación entre el menu principal y la actividad principal

---
+++ app/src/main/java/com/natsh/martin/examplesettings/MainActivity.java
@@ -0,0 +1,37 @@
+package com.natsh.martin.examplesettings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.itm_Ajustes:
+
+                break;
+            case R.id.itm_AcercaDe:
+
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
